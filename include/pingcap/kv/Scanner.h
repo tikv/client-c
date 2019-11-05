37c3fad3ec8be1bf6b972952eb18d482d8a02096
Han Fei <hanfei19910905@gmail.com>
2019-11-05 14:01:20 +0800

    test: add test for scan (#16)
    * test: add test for scan
    
    Signed-off-by: Han Fei <hanfei19910905@gmail.com>
    
    * change test name
    
    Signed-off-by: Han Fei <hanfei19910905@gmail.com>

--- include/pingcap/kv/Scanner.h
+++ include/pingcap/kv/Scanner.h
@@ -3,7 +3,6 @@
 #include <pingcap/Exception.h>
 #include <pingcap/kv/Backoff.h>
 #include <pingcap/kv/Snapshot.h>
-#include <pingcap/kv/Util.h>
 
 namespace pingcap
 {
@@ -38,42 +37,7 @@ struct Scanner
         next();
     }
 
-    // TODO: This function has what kind of exception
-    void next()
-    {
-        Backoffer bo(scanMaxBackoff);
-        if (!valid)
-        {
-            throw Exception("the scanner is valid", LogicalError);
-        }
-
-        for (;;)
-        {
-            idx++;
-            if (idx >= cache.size())
-            {
-                if (eof)
-                {
-                    valid = false;
-                    return;
-                }
-                getData(bo);
-                if (idx >= cache.size())
-                {
-                    continue;
-                }
-            }
-
-            auto current = cache[idx];
-            if (end_key.size() > 0 && current.key() >= end_key)
-            {
-                eof = true;
-                valid = false;
-            }
-
-            return;
-        }
-    }
+    void next();
 
     std::string key()
     {
@@ -90,64 +54,7 @@ struct Scanner
     }
 
 private:
-    void getData(Backoffer & bo)
-    {
-        log->debug("get data for scanner");
-        auto loc = snap.cache->locateKey(bo, next_start_key);
-        auto req_end_key = end_key;
-        if (req_end_key.size() > 0 && loc.end_key.size() > 0 && loc.end_key < req_end_key)
-            req_end_key = loc.end_key;
-
-        auto regionClient = RegionClient(snap.cache, snap.client, loc.region);
-        auto request = new kvrpcpb::ScanRequest();
-        request->set_start_key(next_start_key);
-        request->set_end_key(req_end_key);
-        request->set_limit(batch);
-        request->set_version(snap.version);
-        request->set_key_only(false);
-
-        auto context = request->mutable_context();
-        context->set_priority(::kvrpcpb::Normal);
-        context->set_not_fill_cache(false);
-
-        auto rpc_call = std::make_shared<RpcCall<kvrpcpb::ScanRequest>>(request);
-        regionClient.sendReqToRegion(bo, rpc_call);
-
-        // TODO Check safe point.
-
-        auto responce = rpc_call->getResp();
-        int pairs_size = responce->pairs_size();
-        idx = 0;
-        cache.clear();
-        for (int i = 0; i < pairs_size; i++)
-        {
-            auto current = responce->pairs(i);
-            if (current.has_error())
-            {
-                // process lock
-                throw Exception("has key error", LockError);
-            }
-            cache.push_back(current);
-        }
-
-        log->debug("get pair size: " + std::to_string(pairs_size));
-
-        if (pairs_size < batch)
-        {
-            next_start_key = loc.end_key;
-
-            // If the end key is empty, it infers this region is last and should stop scan.
-            if (loc.end_key.size() == 0 || (next_start_key) >= end_key)
-            {
-                eof = true;
-            }
-
-            return;
-        }
-
-        auto lastKey = responce->pairs(responce->pairs_size() - 1);
-        next_start_key = prefixNext(lastKey.key());
-    }
+    void getData(Backoffer & bo);
 };
 
 // end of namespace.
---
+++ src/test/region_split_scan_test.cc
@@ -0,0 +1,95 @@
+#include <pingcap/Exception.h>
+#include <pingcap/kv/Scanner.h>
+#include <pingcap/kv/Snapshot.h>
+#include <pingcap/kv/Txn.h>
+
+#include "mock_tikv.h"
+
+#include <cassert>
+#include <iostream>
+
+namespace pingcap
+{
+namespace kv
+{
+namespace test
+{
+
+ClusterPtr createCluster(pd::ClientPtr pd_client)
+{
+    kv::RegionCachePtr cache = std::make_shared<kv::RegionCache>(pd_client, "zone", "engine");
+    kv::RpcClientPtr rpc = std::make_shared<kv::RpcClient>();
+    return std::make_shared<Cluster>(pd_client, cache, rpc);
+}
+
+bool testSplitRegionScan()
+{
+    auto mock_kv_cluster = mockkv::initCluster();
+    std::vector<std::string> pd_addrs = mock_kv_cluster->pd_addrs;
+
+    pd::ClientPtr pd_client = std::make_shared<pd::Client>(pd_addrs);
+
+    ClusterPtr control_cluster = createCluster(pd_client);
+    ClusterPtr test_cluster = createCluster(pd_client);
+
+
+    Txn txn(test_cluster);
+
+    txn.set("abc", "1");
+    txn.set("abd", "2");
+    txn.set("abe", "3");
+    txn.set("abf", "4");
+    txn.set("abg", "5");
+    txn.set("abh", "6");
+    txn.set("zzz", "7");
+    txn.commit();
+
+    Snapshot snap(test_cluster->region_cache, test_cluster->rpc_client, pd_client->getTS());
+
+    auto scanner = snap.Scan("", "");
+
+    int answer = 0;
+    while (scanner.valid)
+    {
+        std::cout << "key: " << scanner.key() << std::endl;
+        assert(scanner.value() == std::to_string(++answer));
+        scanner.next();
+    }
+
+    assert(answer == 7);
+
+    answer = 0;
+
+    control_cluster->splitRegion("abe");
+
+    auto scanner1 = snap.Scan("ab", "ac");
+
+    while (scanner1.valid)
+    {
+        std::cout << "key: " << scanner1.key() << std::endl;
+        assert(scanner1.value() == std::to_string(++answer));
+        scanner1.next();
+    }
+    assert(answer == 6);
+
+    return true;
+}
+
+} // namespace test
+} // namespace kv
+} // namespace pingcap
+
+int main(int argv, char ** args)
+{
+    try
+    {
+        if (!pingcap::kv::test::testSplitRegionScan())
+            return 1;
+    }
+    catch (pingcap::Exception & e)
+    {
+        std::cout << e.displayText() << std::endl;
+        return 1;
+    }
+    return 0;
+}
