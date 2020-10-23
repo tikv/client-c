f8236d933f7e3cc3700fb8c56b8f9742e5816031
Tong Zhigao <tongzhigao@pingcap.com>
2020-10-23 17:41:58 +0800

    fix
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- src/test/test_helper.h
+++ src/test/test_helper.h
@@ -13,8 +13,8 @@ using namespace pingcap::kv;
 inline ClusterPtr createCluster(const std::vector<std::string> & pd_addrs)
 {
     ClusterConfig config;
-    config.engine_key = "engine";
-    config.engine_value = "zone";
+    config.tiflash_engine_key = "engine";
+    config.tiflash_engine_value = "tiflash";
     return std::make_unique<Cluster>(pd_addrs, config);
 }
 
