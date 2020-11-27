2b9049b79a130837500dda169da479471f8a398f
Tong Zhigao <tongzhigao@pingcap.com>
2020-11-27 14:38:40 +0800

    Set name for long lived thread (#58)
    * Set name for long lived thread
    
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>
    
    * address comment
    
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

---
+++ include/pingcap/SetThreadName.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#if defined(__APPLE__)
+#include <pthread.h>
+#elif defined(__FreeBSD__)
+#include <pthread.h>
+#include <pthread_np.h>
+#else
+#include <sys/prctl.h>
+#endif
+
+#include <pingcap/Exception.h>
+
+#include <cstring>
+#include <iostream>
+
+namespace pingcap
+{
+inline void SetThreadName(const char * tname)
+{
+    constexpr auto MAX_LEN = 15; // thread name will be tname[:MAX_LEN]
+    if (std::strlen(tname) > MAX_LEN)
+        std::cerr << "set thread name " << tname << " is too long and will be truncated by system\n";
+
+#if defined(__FreeBSD__)
+    pthread_set_name_np(pthread_self(), tname);
+    return;
+
+#elif defined(__APPLE__)
+    if (0 != pthread_setname_np(tname))
+#else
+    if (0 != prctl(PR_SET_NAME, tname, 0, 0, 0))
+#endif
+    throw pingcap::Exception("Cannot set thread name " + std::string(tname), ErrorCodes::LogicalError);
+}
+} // namespace pingcap
