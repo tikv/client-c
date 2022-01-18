651aa2c90ce8ca891000512aaa3e8bddc6ece02e
zanmato1984 <zanmato1984@gmail.com>
2022-01-18 19:53:38 +0800

    Fix ci test script
    Signed-off-by: zanmato1984 <zanmato1984@gmail.com>

--- ci/build-test.sh
+++ ci/build-test.sh
@@ -7,11 +7,11 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SRCPATH=$(cd $SCRIPTPATH/..; pwd -P)
 NPROC=$(nproc || grep -c ^processor /proc/cpuinfo)
 
-if [ -d "$SRCPATH/third_party/kvproto" ]; then
-  cd "$SRCPATH/third_party/kvproto"
-  ./scripts/generate_cpp.sh
-  cd -
-fi
+# if [ -d "$SRCPATH/third_party/kvproto" ]; then
+#   cd "$SRCPATH/third_party/kvproto"
+#   ./scripts/generate_cpp.sh
+#   cd -
+# fi
 
 build_dir="$SRCPATH/build"
 mkdir -p $build_dir && cd $build_dir
