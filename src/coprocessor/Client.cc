dc2610c5642519eb61c4adab65036b19332d084c
guo-shaoge <shaoge1994@163.com>
2022-12-08 16:04:55 +0800

    fix
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- src/coprocessor/Client.cc
+++ src/coprocessor/Client.cc
@@ -216,7 +216,7 @@ std::vector<BatchCopTask> balanceBatchCopTasks(std::vector<BatchCopTask> && orig
                         }
                     }
                 }
-                if (store_id == INVALID_STORE_ID)
+                if (store_id != INVALID_STORE_ID)
                     return store_id;
             }
             for (const auto & store_task : store_task_map)
