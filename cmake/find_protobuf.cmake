824a3886901bddfc7ff2f37fae4fdf38951ccb10
drrtuy <drrtuy@gmail.com>
2025-11-16 22:57:50 +0200

    chore(build): use static protobuf libs for linking.
    Signed-off-by: drrtuy <drrtuy@gmail.com>

--- cmake/find_protobuf.cmake
+++ cmake/find_protobuf.cmake
@@ -1,2 +1,3 @@
+set(Protobuf_USE_STATIC_LIBS ON)
 find_package(Protobuf REQUIRED)
 message(STATUS "Using protobuf: ${Protobuf_VERSION} : ${Protobuf_INCLUDE_DIR}, ${Protobuf_LIBRARY}, ${Protobuf_PROTOC_EXECUTABLE}")
