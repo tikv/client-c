2412ea1ec99aad8230dce6374e462ce95b268be6
Liqi Geng <gengliqiii@gmail.com>
2026-02-02 17:58:48 +0800

    Add TTL mechanism for region cache
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- include/pingcap/pd/MockPDClient.h
+++ include/pingcap/pd/MockPDClient.h
@@ -19,6 +19,8 @@ public:
 
     ~MockPDClient() override = default;
 
+    uint64_t getClusterID() override { return 1; }
+
     uint64_t getGCSafePoint() override { return 10000000; }
 
     uint64_t getGCSafePointV2(KeyspaceID) override { return 10000000; }
